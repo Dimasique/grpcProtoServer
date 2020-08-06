6d565a460c38e0db758c7026eb0236b64f6bb1bf
dimasique <ludaholodec@gmail.com>
2020-08-06 13:54:30 +0400

    init

---
+++ src/main/java/com/example/grpc/GreetingServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.grpc;
+
+import io.grpc.stub.StreamObserver;
+
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+    @Override
+    public void greeting(GreetingServiceOuterClass.HelloRequest request,
+                         StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
+        // HelloRequest has toString auto-generated.
+        System.out.println(request);
+
+        // You must use a builder to construct a new Protobuffer object
+        GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse.newBuilder()
+                .setGreeting("Hello there, " + request.getName())
+                .build();
+
+        System.out.println("built");
+
+        // Use responseObserver to send a single response back
+        responseObserver.onNext(response);
+
+        System.out.println("called next");
+
+        // When you are done, you must call onCompleted.
+        responseObserver.onCompleted();
+
+        System.out.println("called complete");
+    }
+}
